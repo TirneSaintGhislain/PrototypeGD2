15173c3d5520947d87ad4dc0d7c7b5c00a06c9b1
MontyCo <montycorveleyn@gmail.com>
2023-07-19 23:41:16 +0200

    Added Second Method for Evolution and started on Health System

--- Assets/Scripts/EvolutionSystem.cs
+++ Assets/Scripts/EvolutionSystem.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class EvolutionSystem : MonoBehaviour
 {
+    [Header("General Parameters")]
+
+    [SerializeField] private bool _usingMethod1;
     [SerializeField] private int _amountOfAttacks = 3;
+
+    private AttackSystem _attackSystem;
 
     //ideally this is temporary until we can revamp the Attack System
     //This float is used to ensure we don't register the same Attack multiple times
@@ -13,28 +18,35 @@ public class EvolutionSystem : MonoBehaviour
     private bool canAttack = true;
 
     [SerializeField]
-    private int _comboGoal;
-
-    [SerializeField]
     private int _lightHitsIncrease;
-
     [SerializeField]
     private float _heavyAreaIncrease;
-
     [SerializeField]
     private float _dashDamageIncrease;
-
     [SerializeField]
     private int _lightHitsMinimum;
-
     [SerializeField]
     private float _heavyAreaMiminum;
-
     [SerializeField]
     private float _dashDamageMinimum;
 
+    [Header("Method 1")]
+    [SerializeField]
+    private int _attackRatingIncrease = 2;
+    [SerializeField]
+    private int _attackRatingDecrease = 1;
+    [SerializeField]
+    private int _attackEvolveThreshold = 5;
+    [SerializeField]
+    private int _attackDevolveThreshold = -5;
+    private List<int> _attackRatings = new List<int>();
+
+    [Header("Method 2")]
+    [SerializeField]
+    private int _comboGoal;
     [SerializeField]
     private int _unusedLimit;
+    private int _currentCombo;
     private List<int> _timesUnused = new List<int>();
 
     //The combocooldown resets a combo when it doesn't get continued
@@ -42,16 +54,16 @@ public class EvolutionSystem : MonoBehaviour
     private float _comboCoolDown;
     private float _comboTimer;
 
-    private AttackSystem _attackSystem;
-    private int _currentCombo;
 
     // Start is called before the first frame update
     void Start()
     {
         _attackSystem = GetComponent<AttackSystem>();
+        //Initialise the Lists
         for (int i = 0; i < _amountOfAttacks; i++)
         {
             _timesUnused.Add(0);
+            _attackRatings.Add(0);
         }
     }
 
@@ -65,35 +77,84 @@ public class EvolutionSystem : MonoBehaviour
     {
         if (canAttack)
         {
-            //Restart Combo Timer
-            _comboTimer = 0;
-
-            //Increase combo length
-            //Evolve attack when combo goal is reached, then reset combo counter
-            _currentCombo++;
-            if (_currentCombo >= _comboGoal)
+            if (_usingMethod1)
             {
-                EvolveAttack(attackIndex);
-                _currentCombo = 0;
+                UpdateAttackMethod1(attackIndex);
             }
-
-            //Goes over every attack and updates the Unused Amount per attack
-            for (int i = 0; i < _timesUnused.Count; i++)
+            else
             {
-                //If the attack was used it gets reset to 0, otherwise 1 gets added
-                if (i == attackIndex)
-                {
-                    _timesUnused[attackIndex] = 0;
-                }
-                else if (i != attackIndex)
-                {
-                    _timesUnused[i] += 1;
-                }
+                UpdateAttackMethod2(attackIndex);
             }
-
-            CheckIfAttacksNeedToDeEvolve();
-
             StartCoroutine(CountDownAttackCoolDown());
+        }
+    }
+
+    private void UpdateAttackMethod2(int attackIndex)
+    {
+        //Restart Combo Timer
+        _comboTimer = 0;
+
+        //Increase combo length
+        //Evolve attack when combo goal is reached, then reset combo counter
+        _currentCombo++;
+        if (_currentCombo >= _comboGoal)
+        {
+            EvolveAttack(attackIndex);
+            _currentCombo = 0;
+        }
+
+        //Goes over every attack and updates the Unused Amount per attack
+        for (int i = 0; i < _timesUnused.Count; i++)
+        {
+            //If the attack was used it gets reset to 0, otherwise 1 gets added
+            if (i == attackIndex)
+            {
+                _timesUnused[attackIndex] = 0;
+            }
+            else if (i != attackIndex)
+            {
+                _timesUnused[i] += 1;
+            }
+        }
+
+        CheckIfAttacksNeedToDeEvolve();
+    }
+
+    private void UpdateAttackMethod1(int attackIndex)
+    {
+        for (int i = 0; i < _amountOfAttacks; i++)
+        {
+            if (i == attackIndex)
+            {
+                _attackRatings[i] += _attackRatingIncrease;
+            }
+            else
+            {
+                _attackRatings[i] -= _attackRatingDecrease;
+            }
+        }
+
+        //If the ratings reach the thresholds they evolve or devolve
+        ApplyRatings();
+    }
+
+    private void ApplyRatings()
+    {
+        for (int i = 0; i < _attackRatings.Count; i++)
+        {
+            //Evolve the attack if it reaches or exceeds the Evolving Threshold
+            if (_attackRatings[i] >= _attackEvolveThreshold)
+            {
+                EvolveAttack(i);
+                _attackRatings[i] = 0;
+            }
+
+            //Devolve the attack if it reaches or exceeds the Devolving Threshold
+            if (_attackRatings[i] <= _attackDevolveThreshold)
+            {
+                DeEvolveAttack(i);
+                _attackRatings[i] = 0;
+            }
         }
     }
 
---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    [SerializeField]
+    private int _healthMax = 10;
+    [SerializeField]
+    private float _invincibilityTime = 0.3f;
+
+    private bool _canGetHit = true;
+    private int _currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentHealth = _healthMax;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetHit(int damage)
+    {
+        if (_canGetHit)
+        {
+            _currentHealth -= damage;
+
+            if (_currentHealth <= 0)
+            {
+                Die();
+            }
+
+            //Starts the Invincibility Timer
+            StartCoroutine(CountDownInvincibilityTimer());
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log(name + " loses");
+    }
+
+    private IEnumerator CountDownInvincibilityTimer()
+    {
+        _canGetHit = false;
+        yield return new WaitForSeconds(_invincibilityTime);
+        _canGetHit = true;
+    }
+}
